4b12d096f6c5ea52afed9874f12921994e6d7041
Alex Mokin <mr.aleksay@gmail.com>
2020-05-08 10:15:10 +0500

    MAW php1 homework 4 is ready

---
+++ config/main.php
@@ -0,0 +1,10 @@
+<?php
+define("DOCUMENT_ROOT", __DIR__ . "/../");
+define("ENGINE_DIR", DOCUMENT_ROOT . "engine/");
+define("VIEWS_DIR", DOCUMENT_ROOT . "views/");
+define("SITE_DIR", DOCUMENT_ROOT . "public/");
+define("UPLOADS_DIR", SITE_DIR . "img/");
+define("THUMBNAILS_DIR", SITE_DIR . "thumbnails/");
+define("THUMBNAILS_WIDTH", 200);
+define("THUMBNAILS_HEIGHT", 150);
+define("MAX_IMAGE_SIZE", 1500000);
